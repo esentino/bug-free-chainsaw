e65149b716f65ebf2400c6a819399be17f5b1894
Janusz Janowski <januszjanowski@outlook.com>
2018-01-02 23:11:43 +0100

    Add a lot of magic.

--- chainsaw/chain.py
+++ chainsaw/chain.py
@@ -45,11 +45,73 @@ class Board:
             Board({}, {}) with fields:
             {} and lines:
             x {},
-            y {}""".format(self.x_lenght, self.y_lenght, self.board_field, 
+            y {}""".format(self.x_lenght, self.y_lenght, self.board_field,
                            self.x_lines, self.y_lines)
 
     def solve_board(self):
-        self.check_solution()
+        while not self.check_solution():
+            for i, column in enumerate(self.x_lines):
+                x_combinations = Board.make_combination_list(column,
+                                                             self.x_lenght)
+                number_of_combination = len(x_combinations)
+                if number_of_combination == 0:
+                    for field in self.board_field[i]:
+                        if field.probability == 0.0:
+                            field.marked = True
+                    continue
+                axe_probability = 1.0/number_of_combination
+
+                for field in self.board_field[i]:
+                    field.probability = 0.0
+                for combination in x_combinations:
+                    start = 0
+                    match = zip(combination, column)
+                    for (space, axe) in match:
+                        start += space
+                        for x in range(axe):
+                            if not self.board_field[i][start+x].marked:
+                                self.board_field[i][start+x].probability += axe_probability
+                        start += axe
+                if number_of_combination == 0:
+                    for field in self.board_field[i]:
+                        if field.probability == 1.0:
+                            field.marked = True
+            for row in self.y_lines:
+                y_combinations = Board.make_combination_list(row,
+                                                             self.y_lenght)
+            return None
+
+    def make_combination_list(column, lenght):
+        
+        number_of_space = len(column) + 1
+        """
+            OXOXOXO    OXXXOO
+            4 space    2 space
+            3 Axe :)   1 Axe :)
+            Number of space = Number of axe + 1
+        """
+        combination_list = Board.generate(column, lenght, number_of_space)
+        return [combination for combination in combination_list]
+
+    def generate(column, lenght, number_of_space):
+        sum_of_column = sum(column)
+        if number_of_space > 1:
+            combination = [0 for i in range(number_of_space)]
+            end = False
+            while not end:
+                for number in range(number_of_space):
+                    if combination[number] + 1 == lenght:
+                        combination[number] = 0
+                        if number + 1 == number_of_space:
+                            end = True
+                        else:
+                            combination[number+1] += 1
+                    elif number == 0:
+                        combination[number] += 1
+                exclusive_zero = all([x > 0 for x in combination[1:-1]])
+                exclusive_length = sum(combination) + sum_of_column
+                if exclusive_length == lenght and exclusive_zero:
+                    yield combination[:]
 
     def check_solution(self):
         marked_field_count = 0
